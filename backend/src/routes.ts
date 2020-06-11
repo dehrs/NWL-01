40929f249181b93bf4150ddc46697f28283f5292
Débora Rodrigues <44413380+dehrs@users.noreply.github.com>
2020-06-11 18:02:25 -0300

    mobile e frontend

---
+++ backend/src/routes.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import { celebrate, Joi } from "celebrate";
+
+import multer from "multer";
+import multerConfig from "./config/multer";
+
+import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
+
+const routes = express();
+const upload = multer(multerConfig);
+
+const poitsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get("/items", itemsController.index);
+
+routes.post(
+  "/points",
+  upload.single("image"),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email,
+        whatsapp: Joi.number().required,
+        latitude: Joi.number().required,
+        longitude: Joi.number().required,
+        city: Joi.string().required,
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+      }),
+    },
+    {
+      abortEarly: false,
+    }
+  ),
+  poitsController.create
+);
+routes.get("/points", poitsController.index);
+routes.get("/points/:id", poitsController.show);
+
+export default routes;
